3d5ad7e84cbf582e5cd727d5abb30a9c0787799d
Константин Продан <prodan.konstantin@gmail.com>
2023-01-06 14:51:01 +0200

    Збереження громадянина працює

--- src/main/java/ua/od/psrv/simpledoc/server/controllers/Guides/CitizenController.java
+++ src/main/java/ua/od/psrv/simpledoc/server/controllers/Guides/CitizenController.java
@@ -1,9 +1,11 @@
 package ua.od.psrv.simpledoc.server.controllers.Guides;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,9 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lombok.extern.java.Log;
 import ua.od.psrv.simpledoc.server.repository.CitizencategoryRepository;
 import ua.od.psrv.simpledoc.server.repository.CitizenstatusRepository;
@@ -24,6 +23,8 @@ import ua.od.psrv.simpledoc.server.controllers.PrepareController;
 import ua.od.psrv.simpledoc.server.models.SelectedCitizencategory;
 import ua.od.psrv.simpledoc.server.models.SelectedCitizenstatus;
 import ua.od.psrv.simpledoc.server.models.data.Citizen;
+import ua.od.psrv.simpledoc.server.models.data.Citizencategory;
+import ua.od.psrv.simpledoc.server.models.data.Citizenstatus;
 import ua.od.psrv.simpledoc.server.repository.CitizenRepository;
 
 @Log
@@ -107,8 +108,8 @@ public class CitizenController extends PrepareController {
             repository.findAll()
         );
         mav.addObject("edit", id);
-        // getPrestyle().add("tags");
-        // getPostscript().add("tags");
+        getPrestyle().add("tags");
+        getPostscript().add("tags");
 
         List<SelectedCitizenstatus> selectStatuses = new ArrayList<>();
         repositoryStatuses.findAll().forEach(
@@ -148,42 +149,50 @@ public class CitizenController extends PrepareController {
      * @return
      */
     @PostMapping("save")
-    public ModelAndView saveAction(@RequestParam Map<String,Object> model) {
-        ObjectMapper objectMapper = new ObjectMapper();
+    public ModelAndView saveAction(@RequestParam Map<String,Object> model, 
+                                    @RequestParam(name = "Status") Optional<List<Integer>> Status,
+                                    @RequestParam(name = "Category") Optional<List<Integer>> Category) {   
         try {
-            String json = objectMapper.writeValueAsString(model);
-            log.info(json);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        if (model.containsKey("Id")) {
-            try {
-                Citizen item;
-                Long id = Long.parseLong(model.get("Id").toString());
-                String fullname = model.get("Fullname").toString();
-                String address = model.get("Address").toString();
-                Optional<Citizen> itemOptional = repository.findById(id);
-                if (itemOptional.isPresent() && id>0) {
-                    item = itemOptional.get();
-                }
-                else {
-                    item = new Citizen();
-                }
-                Object _Categories = model.get("Category");
-                // List<SelectedCitizencategory> selectCategories = new ArrayList<>();
-                // if (_Categories instanceof Collection) {
-                // selectCategories = new ArrayList<>((Collection<SelectedCitizencategory>)_Categories);
-                // }
-                log.info(_Categories.toString()+"="+_Categories.getClass().getName());
-                item.setFullname(fullname);
-                item.setAddress(address);
-                repository.save(item);
+            Citizen item= new Citizen();
+            if (model.containsKey("Id")) {
+                    Long id = Long.parseLong(model.get("Id").toString());
+                    Optional<Citizen> itemOptional = repository.findById(id);
+                    if (itemOptional.isPresent() && id>0) {
+                        item = itemOptional.get();
+                    }
             }
-            catch  (Exception ex) { 
-                log.warning(ex.getMessage());
+            String fullname = model.get("Fullname").toString();
+            String address = model.get("Address").toString();
+            item.setFullname(fullname);
+            item.setAddress(address);
+            if (Category.isPresent()) {
+                Set<Citizencategory> selectCategories = new HashSet<>();
+                repositoryCategories.findAllById(Category.get()).forEach(
+                    (itemcategory) -> selectCategories.add(itemcategory)
+                );
+                item.setCategory(selectCategories);
             }
+            else {
+                item.setCategory(null);
+            }
+            if (Status.isPresent()) {
+                Set<Citizenstatus> selectStatuses = new HashSet<>();
+                repositoryStatuses.findAllById(Status.get()).forEach(
+                    (itemstatus) -> selectStatuses.add(itemstatus)
+                );
+                item.setStatus(selectStatuses);
+            }
+            else {
+                item.setStatus(null);
+            }
+
+            item=repository.save(item);
+            log.info("Запис збережено! Id="+item.getId());
         }
-        ModelAndView mav = new ModelAndView(rootfolder+"/list");
+        catch  (Exception ex) { 
+            log.warning(ex.getMessage());
+        }
+        ModelAndView mav = new ModelAndView("redirect:/"+rootfolder+"/list");
         prepareNewBundle(mav);
         mav.addObject(
             "list", 
@@ -192,7 +201,7 @@ public class CitizenController extends PrepareController {
         mav.addObject("edit",false);
         return mav;
     }
-    
+
     @GetMapping("delete/{id}")
     public ModelAndView deleteAction(@PathVariable Long id) {
         Optional<Citizen> itemOptional = repository.findById(id);
@@ -201,7 +210,7 @@ public class CitizenController extends PrepareController {
             item.setDeleted(true);
             repository.save(item);
         }
-        ModelAndView mav = new ModelAndView(rootfolder+"/list");
+        ModelAndView mav = new ModelAndView("redirect:/"+rootfolder+"/list");
         prepareNewBundle(mav);
         mav.addObject(
             "list", 
@@ -219,7 +228,7 @@ public class CitizenController extends PrepareController {
             item.setDeleted(false);
             repository.save(item);
         }
-        ModelAndView mav = new ModelAndView(rootfolder+"/list");
+        ModelAndView mav = new ModelAndView("redirect:/"+rootfolder+"/list");
         prepareNewBundle(mav);
         mav.addObject(
             "list", 
